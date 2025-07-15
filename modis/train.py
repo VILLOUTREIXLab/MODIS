be19f7c8db53ff85a461f75b83fd207e24357919
Daniel Lepe-Soltero <dlsoltero@gmail.com>
2025-07-15 12:04:41 +0200

    New file structure

--- modis/train.py
+++ modis/train.py
@@ -1,10 +1,15 @@
+import os
+import pathlib
+
 import torch
 from omegaconf import DictConfig
-
 
 def train(
     config: DictConfig
 ):
-    print(config)
+    num_modalities = len(config.modalities)
+    save_path = pathlib.Path("./saved")
+    checkpoint_path = save_path / "checkpoints" 
 
-
+    print(checkpoint_path)
+    # os.path.join(config.checkpoint_folder, config.model_name)
