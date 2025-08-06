1bed7a099c66fca6c986396fa0a42684618f1858
Daniel Lepe-Soltero <dlsoltero@gmail.com>
2025-08-06 20:21:16 +0200

    Updated __init__ imports

--- modis/__init__.py
+++ modis/__init__.py
@@ -1,3 +1,3 @@
-from .utils.io import load_config
+from .utils.io import load_config, load_checkpoint, load_log
 from .nn import Model
-from .train import train, load_checkpoint, load_log
+from .train import train
